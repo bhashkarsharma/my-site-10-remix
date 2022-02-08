4e20f35f103720b403ee45ce481066367bffdf0f
Bhashkar Sharma <bhashkarsharma@gmail.com>
2022-02-08 01:03:34 +0100

    Basic blog init

---
+++ app/routes/blog/index.tsx
@@ -0,0 +1,31 @@
+import { Link, useLoaderData } from 'remix';
+import type { LoaderFunction } from 'remix';
+import { getPosts, getPublishedLocaleDate } from '~/utils/post';
+import type { Post } from '~/types/post';
+import PageTitle from '~/components/PageTitle';
+
+export const loader: LoaderFunction = () => getPosts();
+
+export default function Blog() {
+  const posts = useLoaderData<Post[]>();
+
+  console.log(posts);
+
+  return (
+    <div className="main-wrapper">
+      <PageTitle>Recent Posts</PageTitle>
+      <ul>
+        {posts.map((post) => (
+          <li className="my-4" key={post.id}>
+            <Link to={`/blog${post.slug}`}>
+              <h2 className="mb-6 text-3xl lg:text-5xl capitalize font-bold">
+                {post.title}
+              </h2>
+              <div>{getPublishedLocaleDate(post.published)}</div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
